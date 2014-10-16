fdb8311f9cffa2783535952ac8a88df794070a82
Christian Frantzen <soulzterx@googlemail.com>
2014-10-16 02:04:06 +0200

    update

---
+++ js/controls.js
@@ -0,0 +1,33 @@
+var obj = {
+    up: function () {
+
+    },
+    down: function () {
+
+    },
+    left: function () {
+
+    },
+    right: function () {
+
+    },
+    enter: function () {
+
+    },
+}
+
+var Controls = (function () {
+    var obj = {};
+    var arrStages = [];
+    var currStage = 0;
+
+    obj.addStage = function (objHandler) {
+        arrStage.push(objHandler);
+    };
+
+    obj.setStage = function (numStg) {
+
+    };
+
+    return obj;
+})();
