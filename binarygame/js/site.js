660bbcdd2473f6e9d987f17176114a913e16a062
Christian Frantzen <soulzterx@googlemail.com>
2015-02-10 20:53:11 +0100

    updt

---
+++ binarygame/js/site.js
@@ -0,0 +1,55 @@
+angular.module("ngBinGameApp", []).
+controller("ngBinGameController", [ "$scope", function ($scope) {
+  var currAnswer = "";
+  var randInt = function (max) {
+    return Math.floor(Math.random()*max);
+  };
+
+  $scope.history = [];
+
+  var genQuestion = function () {
+    var pad8 = function (s) {
+      return Array(8+1-s.length).join("0")+s;
+    };
+    var initNum = randInt(255);
+    if (randInt(1) === 1) {
+      // find binary
+      currAnswer = pad8(initNum.toString(2));
+      $scope.currQuestion = initNum.toString();
+    } else {
+      // find decimal 
+      currAnswer = initNum.toString();
+      $scope.currQuestion = pad8(initNum.toString(2));
+    }
+  };
+  genQuestion();
+
+  $scope.checkAnswer = function () {
+    var result = {
+      q: $scope.currQuestion,
+      a: $scope.strAnswer,
+    };
+    if ($scope.strAnswer === currAnswer) {
+      result.p = 1;
+    } else {
+      result.p = 0;
+    }
+
+    // push answer on the stack
+    // and reset
+    $scope.history.push(result);
+    $scope.strAnswer = "";
+
+    // generate another question
+    genQuestion();
+  };
+
+
+  $scope.getPoints = function () {
+    var points = 0;
+    $scope.history.forEach(function (i) {
+      points += i.p;
+    });
+    return points;
+  };
+}]);
