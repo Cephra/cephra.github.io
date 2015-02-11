cdd9a88bacbeb2609b06f053d05cd557383fb9e3
Christian Frantzen <soulzterx@googlemail.com>
2015-02-11 16:39:41 +0100

    updt

--- binarygame/js/site.js
+++ binarygame/js/site.js
@@ -1,52 +1,69 @@
 angular.module("ngBinGameApp", []).
 controller("ngBinGameController", [ "$scope", function ($scope) {
-  var currAnswer = "";
+  var currAnswer = {};
   var randInt = function (max) {
     return Math.floor(Math.random()*(max+1));
+  };
+  var sanitize = function (s) {
+    var rs = s;
+    rs = rs.substring(rs.length-8);
+    rs = Array(8+1-rs.length).join("0")+rs; 
+    return rs;
   };
   $scope.history = [];
   $scope.strAnswer = ""; 
 
   var genQuestion = function () {
-    var pad8 = function (s) {
-      return Array(8+1-s.length).join("0")+s;
-    };
     var initNum = randInt(255);
+
     if (randInt(1) === 1) {
       // find binary
-      currAnswer = pad8(initNum.toString(2));
+      currAnswer.v = 
+        sanitize(initNum.toString(2));
+      currAnswer.t = "b";
       $scope.currQuestion = initNum.toString();
     } else {
       // find decimal 
-      currAnswer = initNum.toString();
-      $scope.currQuestion = pad8(initNum.toString(2));
+      currAnswer.v = initNum.toString();
+      currAnswer.t = "d";
+      $scope.currQuestion = 
+        sanitize(initNum.toString(2));
     }
   };
   genQuestion();
 
   $scope.checkAnswer = function () {
+    var tmpAnswer = $scope.strAnswer;
+    $scope.strAnswer = "";
+    
+    // answer validation
+    if (tmpAnswer === "") {
+      tmpAnswer = "Ihre Antwort"
+    } else if (currAnswer.t === "b") {
+      // allow for binary answers < 8 bit
+      tmpAnswer = sanitize(tmpAnswer);
+    }
+
+    // object to store the result
     var result = {
       q: $scope.currQuestion,
-      a: $scope.strAnswer,
+      a: tmpAnswer,
       c: "",
     };
-    if ($scope.strAnswer === currAnswer) {
+
+    // check answer
+    if (tmpAnswer === currAnswer.v) {
       result.p = 1;
     } else {
       result.p = 0;
-      result.c = currAnswer;
+      result.c = currAnswer.v;
     }
 
-    // push answer on the stack
-    // and reset
     $scope.history.push(result);
-    $scope.strAnswer = "";
-
-    // generate another question
     genQuestion();
   };
 
-  $scope.resetHistory = function () {
+  $scope.resetGame = function () {
     $scope.history = [];
   };
 
